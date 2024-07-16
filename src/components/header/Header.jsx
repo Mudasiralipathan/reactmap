754a1e8091c5ff1a57ca2adbc0d0c5527a905996
mudasir <mapppali1108@gmail.com>
2024-07-16 01:06:08 -0700

    mee

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -5,13 +5,14 @@ import logo from '../Images/headerlogo.png'
 
 const Header = () => {
      const nav =[    
-      { name: "Home", link: "#",id:1,},
-      { name: "Records", link: "#" ,id:2},
-      { name: "Education", link: "#", id:3},
-      { name: "Contact", link: "#",id:4},
+      { name: "Home", link: ".header",id:1,},
+      { name: "Records", link: "src/components/records/Records" ,id:2},
+      { name: "Contact", link: "#", id:3},
+      { name: "Footer", link: "#",id:4},
       ];
       
   return (
+    
     <div className='header-div'>
         <header className="header">
             <div className="logo"><img src={logo} alt="" /></div>
