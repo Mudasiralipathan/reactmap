753b96c782cb9ce46fbcafac291505702e5890bb
mudasir <mapppali1108@gmail.com>
2024-07-16 01:59:30 -0700

    mm

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -5,10 +5,10 @@ import logo from '../Images/headerlogo.png'
 
 const Header = () => {
      const nav =[    
-      { name: "Home", link: ".header",id:1,},
-      { name: "Records", link: "src/components/records/Records" ,id:2},
-      { name: "Contact", link: "#", id:3},
-      { name: "Footer", link: "#",id:4},
+      { name: "home", link: "",id:1,},
+      { name: "Records", link: "" ,id:2},
+      { name: "Contact", link: "", id:3},
+      { name: "Footer", link: "",id:4},
       ];
       
   return (
